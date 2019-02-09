662b3763021779036b66b4cb17e6ebb67d4c48d6
Dillan Cobb <piysta@yahoo.com>
2019-02-09 17:55:42 -0500

    Add files via upload

---
+++ jquery-ui.js
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+    <title>My jQuery Project</title>
+    
+    <meta charset="utf-8" />
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=3" />
+    
+    <script type="text/javascript" src="jquery.js"></script>
+    
+</head>
+
+<body>
+    
+    <script>
+        
+    </script>
+</body>
+</html>
